d84ff1bbd2475ae0887b2cff1d3f12fc426b9d0a
rbigcal <richardbigcal@gmail.com>
2024-08-12 11:04:32 +0800

    repository code for order and some code updates to pizza

--- Pizza Place Challenge/Core/Data/Entities/Order.cs
+++ Pizza Place Challenge/Core/Data/Entities/Order.cs
@@ -1,24 +1,47 @@
-﻿using Pizza_Place_Challenge.Core.Data.Base;
-using Pizza_Place_Challenge.Core.Enumerations;
+﻿using Microsoft.EntityFrameworkCore;
+using Pizza_Place_Challenge.Core.Data.Base;
 
 namespace Pizza_Place_Challenge.Core.Data.Entities
 {
     public class Order : EntityBase
     {
-        #region . ASSOCIATION FIELDS .
+        #region . ASSOCIATION FIELDS                    .
 
         #endregion
-
-        #region . PROPERTY FIELDS FOR SPECIFIC CLASS .
+        #region . PROPERTY FIELDS FOR SPECIFIC CLASS    .
 
         public DateTime DateTime { get; set; }
 
         #endregion
-        #region . PROPERTY FIELDS THAT CAME FROM CSV .
+        #region . PROPERTY FIELDS THAT CAME FROM CSV    .
         // for data purposes only
         public string OrderID_FROMCSV { get; set; }
         public string Date_FROMCSV { get; set; }
         public string Time_FROMCSV { get; set; }
         #endregion
+    }
+
+    public interface IOrderRepository : IRepository<Order>
+    {
+        Task<IEnumerable<Order>> GetBySpecificDateTime(DateTime datetime);
+        Task<IEnumerable<Order>> GetByDateTimeRange(DateTime mindatetime, DateTime maxdatetime);
+
+    }
+
+    public class OrderRepository : Repository<Order>, IOrderRepository
+    {
+        public OrderRepository(DbContext context) : base(context) { }
+
+        public async Task<IEnumerable<Order>> GetBySpecificDateTime(DateTime datetime)
+        {
+            return await _dbSet.Where(order => order.DateTime == datetime).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Order>> GetByDateTimeRange(DateTime mindatetime, DateTime maxdatetime)
+        {
+            return await _dbSet.Where(order => order.DateTime >= mindatetime && order.DateTime <= maxdatetime).ToListAsync();
+        }
+
+        
     }
 }
--- Pizza Place Challenge/Core/Data/Entities/Pizza.cs
+++ Pizza Place Challenge/Core/Data/Entities/Pizza.cs
@@ -27,26 +27,25 @@ namespace Pizza_Place_Challenge.Core.Data.Entities
 
     }
 
-
     public interface IPizzaRepository : IRepository<Pizza>
     {
         // for specific methods or queries
-        Task<IEnumerable<Pizza>> GetByPizzaType(string pizzaTypeId);
-        Task<IEnumerable<Pizza>> GetPizzaByPriceRange(float minPrice, float maxPrice);
+        Task<IEnumerable<Pizza>> GetByPizzaType(string pizzatypeid);
+        Task<IEnumerable<Pizza>> GetPizzaByPriceRange(float minprice, float maxprice);
     }
 
     public class PizzaRepository : Repository<Pizza>, IPizzaRepository
     {
         public PizzaRepository(DbContext context) : base(context) { }
 
-        public async Task<IEnumerable<Pizza>> GetByPizzaType(string pizzaTypeId)
+        public async Task<IEnumerable<Pizza>> GetByPizzaType(string pizzatypeid)
         {
-            return await _dbSet.Where(pizza => pizza.ID_PizzaType == pizzaTypeId).ToListAsync();
+            return await _dbSet.Where(pizza => pizza.ID_PizzaType == pizzatypeid).ToListAsync();
         }
 
-        public async Task<IEnumerable<Pizza>> GetPizzaByPriceRange(float minPrice, float maxPrice)
+        public async Task<IEnumerable<Pizza>> GetPizzaByPriceRange(float minprice, float maxprice)
         {
-            return await _dbSet.Where(pizza => pizza.Price >= minPrice && pizza.Price <= maxPrice).ToListAsync();
+            return await _dbSet.Where(pizza => pizza.Price >= minprice && pizza.Price <= maxprice).ToListAsync();
         }
     }
 }
